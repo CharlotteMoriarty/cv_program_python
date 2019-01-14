7d9a535bef00f3e06a90f1b6e8e781519fdefa12
CharlotteMoriarty <charlottemoriarty@MacBook-Air-CharlotteMoriarty.local>
2019-01-14 18:44:26 +0100

    create year range search function

--- inputs.py
+++ inputs.py
@@ -56,17 +56,22 @@ def menu():
 
 def search_menu(cv_list):
     loop_handling = True
-    search_choice = input("""
-                1: Wyszukaj po kategorii
-                2: Wyszukaj po roku rozpoczęcia
-                3: Wyszukaj po nazwie stanowiska
-                4: Wyszukaj po nazwie firmy
-                5: Wyjście  
+    while loop_handling:
+        search_choice = input("""
+                    1: Wyszukaj po kategorii
+                    2: Wyszukaj po roku rozpoczęcia
+                    3: Wyszukaj po nazwie stanowiska
+                    4: Wyszukaj po nazwie firmy
+                    5: Wyjście  
     """)
     if search_choice == "1":
         os.system("clear")
         display.logo_print()
         genre_search(cv_list)
+    elif search_choice == "2":
+        os.system("clear")
+        display.logo_print()
+        year_search(cv_list)
 
 
 def genre_search(input_cv):
@@ -86,6 +91,23 @@ def genre_search(input_cv):
             print("Brak kategorii! ")
 
 
+def year_search(input_cv):
+    loop_handling = True
+    while loop_handling:
+        year_range = input("Wpisz interesujący Cię rok (yyyy-yyyy): ")
+        if "-" in year_range:
+            year_range = year_range.split("-")
+            year_range = [int(year) for year in year_range]
+            display.display_table_header()
+            for cv in input_cv:
+                if int(cv[2]) in range(year_range[0], year_range[1] + 1):
+                    display.display_cv_to_print(cv)
+                else:
+                    print("Brak danych w tym okresie")
+                    break
+            loop_handling = False
+        else:
+            print("Wpisz interesujący Cię rok (yyyy-yyyy):  ")
 
 
 def cv_raport():
